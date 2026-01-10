dd1a019a9a7fb6101bfb54618fadfb456dbb534c
creovea-gif <creovea@gmail.com>
2026-01-10 03:10:11 +0100

    Add files via upload

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// إعداد مجلد الملفات الثابتة
+app.use(express.static(__dirname));
+
+// عرض الصفحة الرئيسية
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
